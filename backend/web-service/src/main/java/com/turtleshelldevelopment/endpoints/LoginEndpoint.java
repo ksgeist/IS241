f997334d22ffe9215d2ba29837f8b0fa97030a54
Colink02 <colinkinzel@gmail.com>
2022-09-14 17:52:31 -0500

    Implemented a test version of JWT

--- backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/LoginEndpoint.java
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/LoginEndpoint.java
@@ -1,5 +1,7 @@
 package com.turtleshelldevelopment.endpoints;
 
+import com.auth0.jwt.JWT;
+import com.turtleshelldevelopment.WebServer;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -7,15 +9,21 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.sql.Time;
+import java.time.LocalTime;
+
 public class LoginEndpoint implements Route {
     /***
      * Handles Login
      * Implementation requires a JSON String with username and password keys
+     * example body: {"username": "XXXXX", "password": "XXXXXXX"}
      */
     @Override
     public Object handle(Request request, Response response) {
         JSONObject jsonBody;
         String username, password;
+
+        //Validate Authentication POST Request
         try {
             jsonBody = (JSONObject) new JSONParser().parse(request.body());
             username = (String) jsonBody.get("username");
@@ -32,9 +40,10 @@ public class LoginEndpoint implements Route {
             System.out.println("read");
             System.out.println("user: " + username + ", password: " + password);
             JSONObject success = new JSONObject();
-            success.put("jwt", generateJWTToken());
-            response.body();
+            success.put("jwt", generateJWTToken(username));
+            System.out.println("Output is " + success.toJSONString());
             response.status(200);
+            return success;
         } else {
             response.status(400);
         }
@@ -47,5 +56,10 @@ public class LoginEndpoint implements Route {
     }
 
     private String generateJWTToken(String username) {
+        return JWT.create()
+                .withIssuer("covid-19-dash")
+                .withSubject(username)
+                .withNotBefore(Time.valueOf(LocalTime.now()))
+                .sign(WebServer.JWT_ALGO);
     }
 }
