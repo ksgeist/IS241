442dfc131d823b531de6a0fe276a24741ab69d65
Colink02 <colinkinzel@gmail.com>
2022-09-24 18:36:10 -0500

    Implemented The rest of 2FA Verification for the MFA endpoint

--- backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/MfaEndpoint.java
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/MfaEndpoint.java
@@ -4,26 +4,59 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.turtleshelldevelopment.WebServer;
+import dev.samstevens.totp.code.*;
+import dev.samstevens.totp.time.SystemTimeProvider;
+import dev.samstevens.totp.time.TimeProvider;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Time;
 import java.time.LocalTime;
 
 public class MfaEndpoint implements Route {
     @Override
-    public Object handle(Request request, Response response) {
+    public Object handle(Request request, Response response) throws ParseException {
         String jwtQuery = request.queryParams("jwt");
+        String code = request.queryParams("code");
         DecodedJWT jwt = JWT.decode(jwtQuery);
+        JSONObject jwtPayload = (JSONObject) new JSONParser().parse(jwt.getPayload());
 
         try {
             JSONObject success = new JSONObject();
             WebServer.JWT_ALGO.verify(jwt);
 
+            String username = (String) jwtPayload.get("sub");
+            TimeProvider timeProvider = new SystemTimeProvider();
+            CodeGenerator codeGenerator = new DefaultCodeGenerator(HashingAlgorithm.SHA256);
+            CodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
+
+            //Get Secret from Database
+            PreparedStatement getSecret = WebServer.database.getConnection().prepareStatement("SELECT 2fa_secret FROM User WHERE username = ?;");
+            getSecret.setString(1, username);
+            ResultSet set;
+            if(!(set = getSecret.executeQuery()).next()) {
+                JSONObject error = new JSONObject();
+                error.put("success", false);
+                error.put("message", "Nonexistent user");
+                response.status(401);
+                return error;
+            }
+            String secret = set.getString("2fa_secret");
+
+            if(verifier.isValidCode(secret, code)) {
+                success.put("success", true);
+                success.put("jwt", generateJWTToken(username));
+            }
+
             return success;
-        } catch (SignatureVerificationException e) {
+        } catch (SignatureVerificationException | SQLException e) {
             JSONObject error = new JSONObject();
             error.put("success", false);
             error.put("message", "Invalid Signature");
