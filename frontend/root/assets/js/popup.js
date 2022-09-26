f2e579a3be394b34987a3351d243080cbd4d5420
Katherine Geist <katherine.geist@gmail.com>
2022-09-25 20:59:58 -0500

    fixed the CSS and HTML plus added JJM's stuff

---
+++ frontend/root/assets/js/popup.js
@@ -0,0 +1,20 @@
+/*
+	Missouri COVID-19 Vaccine Tracker Application 
+	Filename: 	popup.js
+	
+	Written by Justin Macapanan
+
+*/ 
+
+window.onload = function(){
+    console.log("hi")
+}
+const submitButton = document.getElementById("login");
+const exit = document.getElementById("exit");
+submitButton.onclick = function(){
+    document.getElementById("pop-up").style.display = "block";
+}
+
+exit.onclick=function(){
+    document.getElementById("pop-up").style.display = "none";
+}
