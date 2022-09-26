8109a3f26f5c6e24103368ecb0fa512d07872ab2
Colink02 <colinkinzel@gmail.com>
2022-09-26 12:38:26 -0500

    Added in 2FA Verification and validation

--- backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/MfaEndpoint.java
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/MfaEndpoint.java
@@ -3,10 +3,12 @@ package com.turtleshelldevelopment.endpoints;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.turtleshelldevelopment.Issuers;
 import com.turtleshelldevelopment.WebServer;
-import dev.samstevens.totp.code.*;
+import dev.samstevens.totp.code.DefaultCodeGenerator;
+import dev.samstevens.totp.code.DefaultCodeVerifier;
+import dev.samstevens.totp.code.HashingAlgorithm;
 import dev.samstevens.totp.time.SystemTimeProvider;
-import dev.samstevens.totp.time.TimeProvider;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -23,19 +25,26 @@ import java.time.LocalTime;
 public class MfaEndpoint implements Route {
     @Override
     public Object handle(Request request, Response response) throws ParseException {
-        String jwtQuery = request.queryParams("jwt");
-        String code = request.queryParams("code");
+        String jwtQuery = request.cookie("token");
+        JSONObject bodyJSON = (JSONObject) new JSONParser().parse(request.body());
+        if(!bodyJSON.containsKey("code")) {
+            JSONObject failure = new JSONObject();
+            failure.put("success", false);
+            failure.put("message", "Missing code in Body");
+            return failure;
+        }
+        String code = (String) bodyJSON.get("code");
         DecodedJWT jwt = JWT.decode(jwtQuery);
-        JSONObject jwtPayload = (JSONObject) new JSONParser().parse(jwt.getPayload());
-
+        WebServer.serverLogger.info("Payload is: " + jwt.getClaims().toString());
         try {
             JSONObject success = new JSONObject();
             WebServer.JWT_ALGO.verify(jwt);
 
-            String username = (String) jwtPayload.get("sub");
-            TimeProvider timeProvider = new SystemTimeProvider();
-            CodeGenerator codeGenerator = new DefaultCodeGenerator(HashingAlgorithm.SHA256);
-            CodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
+            String username = jwt.getSubject();
+            SystemTimeProvider timeProvider = new SystemTimeProvider();
+            DefaultCodeGenerator codeGenerator = new DefaultCodeGenerator(HashingAlgorithm.SHA1, 6);
+            DefaultCodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
+            verifier.setAllowedTimePeriodDiscrepancy(2);
 
             //Get Secret from Database
             PreparedStatement getSecret = WebServer.database.getConnection().prepareStatement("SELECT 2fa_secret FROM User WHERE username = ?;");
@@ -50,11 +59,17 @@ public class MfaEndpoint implements Route {
             }
             String secret = set.getString("2fa_secret");
 
+            WebServer.serverLogger.info("Secret is: " + secret);
+            WebServer.serverLogger.info("Is Code (" + code + ") valid: " + verifier.isValidCode(secret, code));
+
             if(verifier.isValidCode(secret, code)) {
                 success.put("success", true);
-                success.put("jwt", generateJWTToken(username));
+                response.cookie("token", generateJWTToken(username), 300, true, true);
+                return success;
+            } else {
+                success.put("success", false);
+                success.put("message", "Invalid Two Factor Code");
             }
-
             return success;
         } catch (SignatureVerificationException | SQLException e) {
             JSONObject error = new JSONObject();
@@ -68,7 +83,7 @@ public class MfaEndpoint implements Route {
 
     private String generateJWTToken(String username) {
         return JWT.create()
-                .withIssuer("covid-19-dash")
+                .withIssuer(Issuers.AUTHENTICATION.getIssuer())
                 .withSubject(username)
                 .withClaim("mfa", true)
                 .withNotBefore(Time.valueOf(LocalTime.now()))
