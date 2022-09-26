b8cb0eede81f46fba34ba1acae8c5e98d7aea026
Colink02 <colinkinzel@gmail.com>
2022-09-26 03:45:02 -0500

    Further 2FA implementation getting prepared for backend

--- frontend/root/assets/js/handle_login.js
+++ frontend/root/assets/js/handle_login.js
@@ -15,29 +15,42 @@ function handleLoginCall() {
             var json_data = JSON.parse(request.target.responseText);
             if(json_data['success']) {
                 console.log("Success: " + json_data["2faRequired"]);
-                let code = prompt("Please input your Multi-factor Code from your phone.", "");
-                var mfa = new XMLHttpRequest();
-                mfa.open("POST", window.location.origin + "/api/login/mfa");
-                var data = {
-                    "code": code
-                };
-                mfa.onreadystatechange = (request, event) => {
-                    if(request.target.readyState == 4) {
-                        var json_data = JSON.parse(request.target.responseText);
-                        if(json_data['success']) {
-                            console.log("Success: ");
-                            console.log(json_data);
-                            window.location.replace(window.location.origin + "/dashboard.html");
-                        }
-                    }
-                };
-                mfa.send(JSON.stringify(data));
+                //let code = prompt("Please input your Multi-factor Code from your phone.", "");
+                document.getElementById("pop-up").style.display = "block";
+                document.getElementById("pop-up-content").style.display = "block";
             } else {
                 console.log("Error: " + json_data["message"]);
             }
         }
     }
     xhr.send(JSON.stringify(data));
+}
+
+document.getElementById("twofasubmit").onclick = (event) => {
+    event.preventDefault();
+    var code = document.getElementById("twofainput-1").value +
+    document.getElementById("twofainput-2").value + 
+    document.getElementById("twofainput-3").value +
+    document.getElementById("twofainput-4").value +
+    document.getElementById("twofainput-5").value +
+    document.getElementById("twofainput-6").value;
+
+    var mfa = new XMLHttpRequest();
+    mfa.open("POST", window.location.origin + "/api/login/mfa");
+    var data = {
+        "code": code
+    };
+    mfa.onreadystatechange = (request, event) => {
+        if(request.target.readyState == 4) {
+            var json_data = JSON.parse(request.target.responseText);
+            if(json_data['success']) {
+                console.log("Success: ");
+                console.log(json_data);
+                window.location.replace(window.location.origin + "/dashboard.html");
+            }
+        }
+    };
+    mfa.send(JSON.stringify(data));
 }
 
 document.getElementById("login").onclick = (event) => {
--- frontend/root/assets/js/popup.js
+++ frontend/root/assets/js/popup.js
@@ -13,10 +13,23 @@ window.onload = function(){
 const submitButton = document.getElementById("login");
 const exit = document.getElementById("exit");
 
-submitButton.onclick = function(){
-    document.getElementById("pop-up").style.display = "block";
+submitButton.onclick = function() {
 }
 
 exit.onclick=function(){
     document.getElementById("pop-up").style.display = "none";
+    document.getElementById("pop-up-content").style.display = "none";
+}
+
+function handle2FAInput(event) {
+    event.value = event.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+    if(Number(event.value) != NaN && event.value != "") {
+        if(event.nextElementSibling != null) {
+            event.nextElementSibling.focus();
+        }
+    } else if(event.value == "") {
+        if(event.previousElementSibling != null) {
+            event.previousElementSibling.focus();
+        }
+    }
 }
