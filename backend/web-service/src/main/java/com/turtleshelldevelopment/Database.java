cbe1628acdb4ad5436214076539f71d1716e120b
Colink02 <colinkinzel@gmail.com>
2022-09-26 12:41:21 -0500

    Changed url to use .env for url

--- backend/web-service/src/main/java/com/turtleshelldevelopment/Database.java
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/Database.java
@@ -7,14 +7,16 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Database {
-    private static HikariConfig config = new HikariConfig();
+    private static final HikariConfig config = new HikariConfig();
     static HikariDataSource db;
 
 
-    private static String INSERT_USER = "";
-
     public Database() {
-        config.setJdbcUrl("jdbc:mariadb://db.colink02dev.com:3306/is241_mo_vat");
+        config.setMaximumPoolSize(20);
+        config.setConnectionTimeout(300000);
+        config.setConnectionTimeout(120000);
+        config.setLeakDetectionThreshold(300000);
+        config.setJdbcUrl("jdbc:mariadb://" + WebServer.env.get("DB_URL") + "/is241_mo_vat");
         config.setUsername(WebServer.env.get("DB_USERNAME", "db_team"));
         config.setPassword(WebServer.env.get("DB_PASSWORD", ""));
         db = new HikariDataSource(config);
