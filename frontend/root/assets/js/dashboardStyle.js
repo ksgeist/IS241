6d322f69f904d7f788e448dd997a07cac91274e9
Macapanpan <jmacapanpan@my.stlcc.edu>
2022-10-11 20:40:16 -0500

    very glitchy attempt at javascript for dashboard buttons

--- frontend/root/assets/js/dashboardStyle.js
+++ frontend/root/assets/js/dashboardStyle.js
@@ -1,17 +1,28 @@
+var dashboardArray = document.getElementsByClassName("dashboard");
 window.onload = function () {
 
-    console.log();
+    //console.log(dashboardArray.length);
+    //console.log(dashboardArray.children)
   };
 
-// TEMPORARY dummy hrefs TEMPORARY 
-var dashboardArray = document.getElementsByClassName("dashboard");
-for(const dash of dashboardArray ){
-    //let l = 1/(100/ dashboardArray.length + 1);
-    for(const child of dash.children){
-        
-        child.setAttribute('href', "dummy.html")
-        //child.setAttribute("style", `width:f{}`)
+function resize(){
+    for(const dash of dashboardArray ){
+        console.log(dash.children.length);
+        let l = (100/ (dash.children.length ));
+        //console.log(dash.length);
+        //console.log(l)
+        let counter = 1
+        for(const child of dash.children){
+            // TEMPORARY dummy hrefs TEMPORARY 
+            child.setAttribute('href', "dummy.html")
+
+            child.setAttribute("style", `margin-left:${l*counter}%`)
+            counter++;
+
+        }
     }
-}
 
+};
 
+resize(); 
+window.onresize = resize();
