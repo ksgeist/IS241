384262469cc84243e9a710374f132f10c16325cd
Colink02 <colinkinzel@gmail.com>
2022-10-15 16:12:46 -0500

    updated map to display the missouri bounds

--- frontend/root/assets/js/create_site/site.js
+++ frontend/root/assets/js/create_site/site.js
@@ -2,6 +2,7 @@ proj4.defs("ESRI:102696","+proj=tmerc +lat_0=35.8333333333333 +lon_0=-90.5 +k=0.
 proj4.defs("ESRI:102697","+proj=tmerc +lat_0=35.8333333333333 +lon_0=-92.5 +k=0.999933333333333 +x_0=500000.000000001 +y_0=0 +datum=NAD83 +units=us-ft +no_defs +type=crs");
 proj4.defs("ESRI:102698","+proj=tmerc +lat_0=36.1666666666667 +lon_0=-94.5 +k=0.999941176470588 +x_0=850000 +y_0=0 +datum=NAD83 +units=us-ft +no_defs +type=crs");
 ol.proj.proj4.register(proj4);
+
 var map = new ol.Map({
   target: 'map',
   layers: [
@@ -11,12 +12,28 @@ var map = new ol.Map({
   ],
   view: new ol.View({
     //MIN X, Y, MAX X, Y
+    showFullExtent: true,
     extent: [-10661067.63327181, 4297869.751737669, -9918566.629680675, 4954985.832353976],
     center: [-10382285.999155946,4273342.734820133],
     projection: "EPSG:3857",
     zoom: 1
   })
 });
+
+const stateLayer = new ol.layer.VectorImage({
+  source: new ol.source.Vector({
+    url: "assets/us-state-boundaries.geojson",
+    format: new ol.format.GeoJSON()
+  }),
+  visible: true,
+  style: new ol.style.Style({
+    fill: new ol.style.Stroke({
+      color: [0, 0, 255, 0.25],
+    })
+  })
+});
+map.addLayer(stateLayer);
+
 map.on('postcompose',function(e){
-  document.querySelector('canvas').style.filter="invert(90%)";
+  //document.querySelector('canvas').style.filter="invert(90%)";
 });
