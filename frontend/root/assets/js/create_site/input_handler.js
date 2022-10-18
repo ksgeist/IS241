90cf50841a4f7d9cd252008a59510c85a15268a6
Colink02 <colinkinzel@gmail.com>
2022-10-18 02:26:33 -0500

    Added in a few more handling for form elements in create_site
    Took 45 seconds

--- frontend/root/assets/js/create_site/input_handler.js
+++ frontend/root/assets/js/create_site/input_handler.js
@@ -118,39 +118,52 @@ const counties = [
 
 var list = "";
 for (county in counties) {
-    list += '<option value="' + counties[county]["name"] + '\">'
-    
+  list += '<option value="' + counties[county]["name"] + '">';
 }
 document.getElementById("counties-list").innerHTML = list;
 
-document.getElementById("address-autocomplete").onblur = function () {
+function displayOnMap() {
+  if (
+    document.getElementById("address").value == "" ||
+    document.getElementById("city").value == "" ||
+    document.getElementById("zip").value == ""
+  )
+    return;
   fetch(
-    `${window.location.hostname}/lookupAddress?address=${
-      document.getElementById("address-autocomplete").value
+    `${window.location.origin}/api/lookupAddress?address=${
+      document.getElementById("address").value
+    }&city=${document.getElementById("city").value}&zip=${
+      document.getElementById("zip").value
     }`
   ).then(async (resp) => {
     var info = await resp.json();
     console.log(info);
     if (
-      info.hasOwnProperty("addressMatches") &&
-      info["addressMatches"].length > 0
+      info.hasOwnProperty("result") &&
+      info["result"]["addressMatches"].length > 0
     ) {
       var markerLoc = new ol.geom.Point(
         proj4("EPSG:4326", "EPSG:3857", [
-          info["addressMatches"][0]["coordinates"]["x"],
-          info["addressMatches"][0]["coordinates"]["y"],
+          info["result"]["addressMatches"][0]["coordinates"]["x"],
+          info["result"]["addressMatches"][0]["coordinates"]["y"],
         ]),
         "EPSG:3857"
       );
       console.log(markerLoc);
       var marker = new ol.Feature(markerLoc);
       console.log(marker);
+      markers.getSource().clear();
       markers.getSource().addFeature(marker);
       map.getView().fit(markerLoc.extent_, map.getSize());
       map.getView().setZoom(17);
     }
   });
-};
+}
+
+document.getElementById("address").onblur = displayOnMap;
+document.getElementById("city").onblur = displayOnMap;
+document.getElementById("zip").onblur = displayOnMap;
+
 document.getElementById("county").onchange = function (ele) {
   counties.forEach((county) => {
     if (ele.currentTarget.value == county["name"]) {
@@ -160,46 +173,79 @@ document.getElementById("county").onchange = function (ele) {
   });
 };
 
-
 const isNumericInput = (event) => {
-    const key = event.keyCode;
-    return ((key >= 48 && key <= 57) || // Allow number line
-        (key >= 96 && key <= 105) // Allow number pad
-    );
+  const key = event.keyCode;
+  return (
+    (key >= 48 && key <= 57) || // Allow number line
+    (key >= 96 && key <= 105) // Allow number pad
+  );
 };
 
 const isModifierKey = (event) => {
-    const key = event.keyCode;
-    return (event.shiftKey === true || key === 35 || key === 36) || // Allow Shift, Home, End
-        (key === 8 || key === 9 || key === 13 || key === 46) || // Allow Backspace, Tab, Enter, Delete
-        (key > 36 && key < 41) || // Allow left, up, right, down
-        (
-            // Allow Ctrl/Command + A,C,V,X,Z
-            (event.ctrlKey === true || event.metaKey === true) &&
-            (key === 65 || key === 67 || key === 86 || key === 88 || key === 90)
-        )
+  const key = event.keyCode;
+  return (
+    event.shiftKey === true ||
+    key === 35 ||
+    key === 36 || // Allow Shift, Home, End
+    key === 8 ||
+    key === 9 ||
+    key === 13 ||
+    key === 46 || // Allow Backspace, Tab, Enter, Delete
+    (key > 36 && key < 41) || // Allow left, up, right, down
+    // Allow Ctrl/Command + A,C,V,X,Z
+    ((event.ctrlKey === true || event.metaKey === true) &&
+      (key === 65 || key === 67 || key === 86 || key === 88 || key === 90))
+  );
 };
 
 const enforceFormat = (event) => {
-    // Input must be of a valid number format or a modifier key, and not longer than ten digits
-    if(!isNumericInput(event) && !isModifierKey(event)){
-        event.preventDefault();
-    }
+  // Input must be of a valid number format or a modifier key, and not longer than ten digits
+  if (!isNumericInput(event) && !isModifierKey(event)) {
+    event.preventDefault();
+  }
 };
 
 const formatToPhone = (event) => {
-    if(isModifierKey(event)) {return;}
+  if (isModifierKey(event)) {
+    return;
+  }
 
-    const input = event.target.value.replace(/\D/g,'').substring(0,10); // First ten digits of input only
-    const areaCode = input.substring(0,3);
-    const middle = input.substring(3,6);
-    const last = input.substring(6,10);
+  const input = event.target.value.replace(/\D/g, "").substring(0, 10); // First ten digits of input only
+  const areaCode = input.substring(0, 3);
+  const middle = input.substring(3, 6);
+  const last = input.substring(6, 10);
 
-    if(input.length > 6){event.target.value = `(${areaCode}) ${middle} - ${last}`;}
-    else if(input.length > 3){event.target.value = `(${areaCode}) ${middle}`;}
-    else if(input.length > 0){event.target.value = `(${areaCode}`;}
+  if (input.length > 6) {
+    event.target.value = `(${areaCode}) ${middle} - ${last}`;
+  } else if (input.length > 3) {
+    event.target.value = `(${areaCode}) ${middle}`;
+  } else if (input.length > 0) {
+    event.target.value = `(${areaCode}`;
+  }
 };
 
-const inputElement = document.getElementById('phoneNumber');
-inputElement.addEventListener('keydown',enforceFormat);
-inputElement.addEventListener('keyup',formatToPhone);
+const inputElement = document.getElementById("phoneNumber");
+inputElement.addEventListener("keydown", enforceFormat);
+inputElement.addEventListener("keyup", formatToPhone);
+
+document.getElementById("newSite").onsubmit = function (event) {
+  event.preventDefault();
+  //Handle Address Validation
+  var address = document.getElementById("address");
+  var city = document.getElementById("city");
+  var zipCode = document.getElementById("zip");
+  var county = document.getElementById("county");
+  var fips = document.getElementById("fips");
+  var phone = document.getElementById("phone");
+
+  fetch(`
+    ${window.location.origin}/site/create/?
+    address=${address}&
+    city=${city}&
+    zip=${zipCode}&
+    county=${county}&
+    fips=${fips}&
+    phone=${phone}`).then(async (resp) => {
+      
+    });
+};
