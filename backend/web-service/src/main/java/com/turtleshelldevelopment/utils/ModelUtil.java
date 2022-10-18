e21faa8fb9ceeebfb9d4f4e8ec1deb8bcbc5791d
Colink02 <colinkinzel@gmail.com>
2022-10-18 02:34:27 -0500

    Added new utility class to handle returning data in a JSON format
    Took 36 seconds

---
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/utils/ModelUtil.java
@@ -0,0 +1,58 @@
+package com.turtleshelldevelopment.utils;
+
+import com.turtleshelldevelopment.PermissionType;
+import com.turtleshelldevelopment.WebServer;
+import org.json.simple.JSONObject;
+import spark.utils.IOUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelUtil {
+
+    private static final String headerFile;
+
+    static {
+        try {
+            headerFile = IOUtils.toString(WebServer.class.getResourceAsStream("/header.html"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private final JSONObject modelData = new JSONObject();
+
+    public ModelUtil() {
+        modelData.put("header", headerFile);
+    }
+
+    public ModelUtil addPermissions(HashMap<PermissionType, Boolean> perms) {
+        if(perms == null) return this;
+        for(Map.Entry<PermissionType, Boolean> permission : perms.entrySet()) {
+            modelData.put(permission.getKey().getVal().toLowerCase(), permission.getValue());
+        }
+        return this;
+    }
+    public ModelUtil add(String key, Object value) {
+        modelData.put(key, value);
+        return this;
+    }
+
+    public ModelUtil addError(int errorCode, String errorMessage) {
+        modelData.put("error_code", errorCode);
+        modelData.put("error_message", errorMessage);
+        return this;
+    }
+
+    public ModelUtil addMFAError(boolean success, String message, boolean shouldRetry) {
+        modelData.put("success", success);
+        modelData.put("messager", message);
+        modelData.put("retry", shouldRetry);
+        return this;
+    }
+
+    public JSONObject build() {
+        return this.modelData;
+    }
+}
