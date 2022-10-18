09406a0e03f031b5a1cb020f8c725988d7c37822
Colink02 <colinkinzel@gmail.com>
2022-10-18 02:21:10 -0500

    Added /AddRecord Page to Backend
    Took 27 seconds

---
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/pages/AddRecordPage.java
@@ -0,0 +1,15 @@
+package com.turtleshelldevelopment.pages;
+
+import com.turtleshelldevelopment.utils.ModelUtil;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.template.velocity.VelocityTemplateEngine;
+
+public class AddRecordPage implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return new VelocityTemplateEngine().render(new ModelAndView(new ModelUtil().build(), "/frontend/add_record.vm"));
+    }
+}
