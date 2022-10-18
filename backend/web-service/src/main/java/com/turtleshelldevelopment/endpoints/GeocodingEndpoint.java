162537a39df4171b91b3b0d814742184827954c2
Colink02 <colinkinzel@gmail.com>
2022-10-18 02:30:15 -0500

    Added a bit more precise way of finding the correct location on the map
    Took 47 seconds

--- backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/GeocodingEndpoint.java
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/GeocodingEndpoint.java
@@ -7,22 +7,30 @@ import spark.Route;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 public class GeocodingEndpoint implements Route {
     private final HttpClient client = HttpClient.newHttpClient();
-    private final String GEOCODING_URL = "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=%s&benchmark=4&format=json";
 
     @Override
     public Object handle(Request request, Response response) throws IOException, InterruptedException, URISyntaxException {
         String addressLookup = request.queryParams("address");
+        String city = request.queryParams("city");
+        String zip = request.queryParams("zip");
+        if(city == null) city = "";
+        if(zip == null) zip = "";
 
-        HttpRequest serviceRequest = HttpRequest.newBuilder().uri(new URI(String.format(GEOCODING_URL, addressLookup))).GET().build();
+        String GEOCODING_URL = "https://geocoding.geo.census.gov/geocoder/locations/address?street=%s&state=Missouri&city=%s&zip=%s&benchmark=Public_AR_Current&format=json";
+        HttpRequest serviceRequest = HttpRequest.newBuilder().uri(new URI(String.format(GEOCODING_URL, URLEncoder.encode(addressLookup, StandardCharsets.UTF_8), URLEncoder.encode(city, StandardCharsets.UTF_8),  URLEncoder.encode(zip, StandardCharsets.UTF_8)))).GET().build();
         HttpResponse<String> serviceResponse = client.send(serviceRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Service response is " + serviceResponse.body());
         response.body(serviceResponse.body());
         response.status(serviceResponse.statusCode());
-        return "";
+        response.header("Content-Type", "application/json");
+        return serviceResponse.body();
     }
 }
