967621a8886be602b01c6b997c8be2625537eb93
Colink02 <colinkinzel@gmail.com>
2022-10-27 00:12:04 -0500

    Moved vm files
    Took 4 seconds

---
+++ frontend/root/assets/js/users/add_user.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const successModal = new bootstrap.Modal("#successModal");
+
+successModal.toggle();
+
+function submitForm() {
+    var xhr = new XMLHttpRequest();
+    var firstName = document.getElementByID("");
+    var lastName = document.getElementById("");
+
+}
