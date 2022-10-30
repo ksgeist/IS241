8a6eb4a37ce6f4df04c383c500b0e237266660c0
Colink02 <colinkinzel@gmail.com>
2022-10-30 05:41:10 -0500

    Added counties to site create model

--- backend/web-service/src/main/java/com/turtleshelldevelopment/pages/SiteCreatePage.java
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/pages/SiteCreatePage.java
@@ -1,17 +1,18 @@
 package com.turtleshelldevelopment.pages;
 
 import com.turtleshelldevelopment.utils.ModelUtil;
-import org.json.JSONObject;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.Map;
+
 public class SiteCreatePage implements Route {
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        JSONObject modelInfo = new ModelUtil().build();
+        Map<String, Object> modelInfo = new ModelUtil().addCounties().build();
         return new VelocityTemplateEngine().render(new ModelAndView(modelInfo, "/frontend/create_site.vm"));
     }
 }
