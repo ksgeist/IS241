da53e9b4d1cae203de27b9a03a30991dbe4329eb
Colink02 <colinkinzel@gmail.com>
2022-10-31 11:23:47 -0500

    Cleaned up code

--- backend/web-service/src/main/java/com/turtleshelldevelopment/pages/PrintInfoPage.java
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/pages/PrintInfoPage.java
@@ -1,108 +1,29 @@
-package com.turtleshelldevelopment;
+package com.turtleshelldevelopment.pages;
 
-import com.turtleshelldevelopment.utils.ModelUtil;
-import spark.ModelAndView;
+import com.turtleshelldevelopment.BackendServer;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
-import java.sql.*;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class PrintInfo{
+public class PrintInfoPage implements Route {
 
-@Override
-    public Object handle(Request request, Response response) {
-        //
-        String stringQuery = "SELECT * 
-            FROM PatientInformation p, Vaccine v, Site s
-            WHERE v.patient_id_vaccine = p.patient_id AND 
-            s.counties_site = v.site_id
-            p.last_ss_num = ? AND p.last_name = ?";
-        if(request.queryParams("dob") != null){
-            stringQuery+= "p.dob = ?";
-        }
-        PreparedStatement patientSearch = BackendServer.database.getConnection().prepareStatement(stringQuery+=";");
+    @Override
+    public Object handle(Request request, Response response) throws SQLException {
+        try(PreparedStatement patientSearch = BackendServer.database.getConnection().prepareCall("CALL SEARCH_PATIENTS(?, ?, ?)")) {
+            patientSearch.setString(1, request.queryParams("ss4"));
+            patientSearch.setString(2, request.queryParams("lname"));
+            patientSearch.setDate(3, Date.valueOf(request.queryParams("dob")));
+            ResultSet set = patientSearch.executeQuery();
 
-        patientSearch.setString(1, request.queryParams("ss4"));
-        patientSearch.setString(2, request.queryParams("lname"));
-        if(request.queryParams("dob" != null)){
-            patientSearch.setString(3, request.queryParams("dob"));
+        } catch (SQLException e) {
+
         }
 
-        ResultSet set = patientSearch.executeQuery();
-       
-
-    //columns and formatKey function are 
-    //here only to output a quick/simple html
-    //document to be printed
-    String[] columns = new String[]
-    {
-       "patient_id",
-       "first_name",
-       "middle_name",
-       "last_name",
-       "last_ss_num",
-       "dob",
-       "email",
-
-       "lot_num",
-       "site_id",
-       "patient_id",
-       "administered_date",
-       "manufactured",
-       "dose",
-       "administrated_by" 
-
-       ,"site"
-    };
-
-    String formatKey(String str){
-        String placeholder = str.substring(0, 1).toUpperCase + str.substring(1);
-        for(int i = 0; i< placeholder.length(); i++){
-            if(str[i] == "_"){
-                str[i] = " ";
-            }
-        }
-    }
-
-    //Set Response.body, uses the columns and formatKey
-    //because I was just trying to get something done quick
-    //and that's what I had.
-    response.body(
-        () -> {
-            String printOutput;
-            for(int i = 0; i<15; i++)
-            {
-                //This is where the executed query gets its columns extracted
-                //at "set.getString"
-                //and subsequently it's turned into a rudimentary html layout
-                printOutput+="<h1>"+formatKey(columns[i])+":"+"</h1>"
-                +"<h2>"+set.getString(columns[i]) + "</h2>";
-            }
-        }
-    );
-
-
-
-
-
+        return "";
     }
 }
-
-
-/*
-    public Object handle(Request request, Response response) {
-       @Override
-        PreparedStatement patientSearch = BackendServer.database.getConnection().prepareStatement(
-            "SELECT * 
-            FROM PatientInformation p
-            LEFT OUTER JOIN Vaccine v 
-            ON v.patient_vaccine_id = p.patient_id
-             WHERE"
-            + request.queryParams("ss4") +" = p.last_ss_num AND"
-            + request.queryParams("lname") +" = p.last_name;"
-        );
-        ResultSet set = patientSearch.executeQuery()
-       }
-*/
-// BackendServer.db.getConnection
---
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/pages/PrintRecordPage.java
@@ -0,0 +1,13 @@
+package com.turtleshelldevelopment.pages;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class PrintRecordPage implements Route {
+    @Override
+    public Object handle(Request request, Response response) {
+
+        return null;
+    }
+}
