8ed3d62328ae4dfc7a55c76601287af16f1dac21
Colink02 <colinkinzel@gmail.com>
2022-11-01 04:00:38 -0500

    Added a simple constants class to hold some info about info that the client/web browser has

---
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/Constants.java
@@ -0,0 +1,7 @@
+package com.turtleshelldevelopment;
+
+import java.util.List;
+
+public class Constants {
+    public static List<String> CLIENT_SEX_IDENTITIES = List.of("Female", "Male", "Non-Binary");
+}
