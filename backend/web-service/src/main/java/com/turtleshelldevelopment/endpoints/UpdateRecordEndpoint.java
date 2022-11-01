e51b164132e92778bf32eeb5e798dedb0ea85a5f
Colink02 <colinkinzel@gmail.com>
2022-11-01 03:58:18 -0500

    Added start of implementation of update record

--- backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/UpdateRecordEndpoint.java
+++ backend/web-service/src/main/java/com/turtleshelldevelopment/endpoints/UpdateRecordEndpoint.java
@@ -1,5 +1,7 @@
 package com.turtleshelldevelopment.endpoints;
 
+import com.turtleshelldevelopment.Constants;
+import com.turtleshelldevelopment.utils.ResponseUtils;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -7,6 +9,26 @@ import spark.Route;
 public class UpdateRecordEndpoint implements Route {
     @Override
     public Object handle(Request request, Response response) throws Exception {
+        String lastName = request.params("lname");
+        String firstName = request.params("fname");
+        String middleName = request.params("mname");
+        String sexIdentity = request.params("sex");
+        if(lastName.length() > 45) {
+            return ResponseUtils.createError("Last Name too long (max 45 characters)", 400, response);
+        }
+        if(firstName.length() > 45) {
+            return ResponseUtils.createError("First Name too long (max 45 characters)", 400, response);
+        }
+        if(middleName.length() > 45) {
+            return ResponseUtils.createError("Middle Name too long (max 45 characters)", 400, response);
+        }
+        if(!Constants.CLIENT_SEX_IDENTITIES.contains(sexIdentity)) {
+            return ResponseUtils.createError("Invalid Sex Identity", 400, response);
+        }
+
+        //TODO write stored procedure to take in these values and update them if need be (ignore values that are null/empty strings)
+
+
         return "";
     }
 }
