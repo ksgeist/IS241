424a95fa4dbb43d82b8f1c62534374656871f055
Katherine Geist <katherine.geist@gmail.com>
2022-11-06 17:54:12 -0600

    Created Print Daily Record page recycling add.css styles and using Bootstrap Date Picker to be able to restrict choice to what we want

---
+++ frontend/root/assets/js/vax_queries/daily-record-date-picker.js
@@ -0,0 +1,20 @@
+"use strict";
+/*
+	Missouri COVID-19 Vaccine Tracker Application 
+	Filename: 	daily-record-date-picker.js
+	
+	Written by Katherine Geist, to set the Daily Record date picker parameters
+
+*/
+
+$(document).ready(function(){
+ $('#datepicker').datepicker({
+  "format": "mm/dd/yyyy",
+  "startDate": "-8d",
+  "endDate": "-1d",
+  "keyboardNavigation": true
+ }); 
+});
+
+// If you need to extract the date using date picker
+myDate = $('#datepicker').datepicker('getDate');
